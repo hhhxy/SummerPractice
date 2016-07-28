5ae80449f1ae38835fb3a56138afd3e2cad692df
hhhxy <1090731165@qq.com>
2016-07-28 20:54:13 +0800

    finish FindScore

---
+++ SecondWeek/FindScore/js/class-data.js
@@ -0,0 +1,42 @@
+var classData = [{
+	value: "11000",
+	text: '第一学年',
+	children: [{
+		value: "11001",
+		text: "上学期"
+	},{
+		value: "11002",
+		text: "下学期"
+	}]
+},{
+	value: "12000",
+	text: '第二学年',
+	children: [{
+		value: "12001",
+		text: "上学期"
+	},{
+		value: "12002",
+		text: "下学期"
+	}]
+},{
+	value: "13000",
+	text: '第三学年',
+	children: [{
+		value: "13001",
+		text: "上学期"
+	},{
+		value: "13002",
+		text: "下学期"
+	}]
+},
+	{
+	value: "14000",
+	text: '第四学年',
+	children: [{
+		value: "14001",
+		text: "上学期"
+	},{
+		value: "14002",
+		text: "下学期"
+	}]
+}]
